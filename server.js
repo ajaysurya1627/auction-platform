14977bf3159604f4fe021acbdf60531ad850bfe0
ajaysurya1627 <eajaysurya1627@gmail.com>
2025-04-15 05:57:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,17 @@ const express = require("express");
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware to parse JSON (should be before routes)
 app.use(express.json());
 
-// Default route
 app.get("/", (req, res) => {
     res.send("Welcome to the Auction Platform! ");
 });
 
-// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/auctions", require("./routes/auctionRoutes"));
 
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
